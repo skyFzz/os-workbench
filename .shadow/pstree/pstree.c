dae0e0fac5dc2b437ec8d106cfcaaf3a83aa2a5c
tracer-nju <tracer@nju.edu.cn>
2025-02-03 01:16:00 -0500

    Linux hz-TM1701 6.8.0-52-generic #53-Ubuntu SMP PREEMPT_DYNAMIC Sat Jan 11 00:06:25 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  01:16:00 up 1 day, 13:54,  1 user,  load average: 0.27, 0.29, 0.36

--- .shadow/pstree/pstree.c
+++ .shadow/pstree/pstree.c
@@ -141,7 +141,7 @@ void getNodes(struct Node *hashmap) {
 					printf("pid is %s\n", pid);
 					ret = fclose(fp);
 					assert(ret == 0);
-					memset(pid, 0, 8);
+					i = 0;	// reset
 				}
 				subent = readdir(subdir);
 			}		
