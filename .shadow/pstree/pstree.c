15eefcb77b29a1e2e3d961362af47539b045acda
tracer-nju <tracer@nju.edu.cn>
2025-02-05 13:04:51 -0500

    Linux hz-TM1701 6.8.0-52-generic #53-Ubuntu SMP PREEMPT_DYNAMIC Sat Jan 11 00:06:25 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  13:04:51 up 4 days,  1:43,  1 user,  load average: 0.48, 0.59, 0.54

--- .shadow/pstree/pstree.c
+++ .shadow/pstree/pstree.c
@@ -172,6 +172,7 @@ struct List *makeLists() {
 					tmp.tail->next->next = node;
 					tmp.tail->next = node;
 					node->next = NULL;
+					printf("The name is %s\n", tmp.head->next->name);
 								
 					ret = fclose(fp);
 					assert(ret == 0);
@@ -189,7 +190,7 @@ struct List *makeLists() {
 		entry = readdir(dir);	
 	}
 	assert(errno == 0);
-	printf("The name is %s\n", tmp.head->next->name);
+	printf("hi");
 
 	ret = closedir(dir);
 	assert(ret == 0);
