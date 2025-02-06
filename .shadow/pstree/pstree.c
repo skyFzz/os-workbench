7dd929efe529d663595f9415ecc01aeb0bcc6061
tracer-nju <tracer@nju.edu.cn>
2025-02-06 08:55:08 -0500

    Linux hz-TM1701 6.8.0-52-generic #53-Ubuntu SMP PREEMPT_DYNAMIC Sat Jan 11 00:06:25 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  08:55:08 up 4 days, 21:33,  1 user,  load average: 0.00, 0.05, 0.13

--- .shadow/pstree/pstree.c
+++ .shadow/pstree/pstree.c
@@ -245,7 +245,7 @@ struct Node *makeTree(struct List *lists) {
 			}
 			// find mom
 			printf("child name %s\n", child->name);
-			printf("child ppid%d\n", child->ppid);
+			printf("child ppid %d\n", child->ppid);
 			for (mom = lists[hash(child->ppid)].head; mom != NULL; mom = mom->next) {
 				mom = mom->next;
 				if (mom->pid == child->ppid) {
@@ -256,6 +256,7 @@ struct Node *makeTree(struct List *lists) {
 					} else {
 						printf("mom name is %s\n", mom->name);
 						tmp = mom->fborn;
+						printf("he\n");
 						while (tmp->sib != NULL) {
 							printf("hi\n");
 							tmp = tmp->sib;
