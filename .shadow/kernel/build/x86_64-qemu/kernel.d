c854ebb7c3f7d47f2e243160a0db32226a57d741
tracer-nju <tracer@nju.edu.cn>
2025-02-11 02:46:15 -0500

    Linux hz-TM1701 6.8.0-52-generic #53-Ubuntu SMP PREEMPT_DYNAMIC Sat Jan 11 00:06:25 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  02:46:15 up 16:46,  1 user,  load average: 0.76, 0.55, 0.58

---
+++ .shadow/kernel/build/x86_64-qemu/kernel.d
@@ -0,0 +1,8 @@
+/home/hz/Laser/os-workbench/kernel/build/x86_64-qemu/kernel.o: \
+ /home/hz/Laser/os-workbench/kernel/kernel.c \
+ /home/hz/Laser/os-workbench/kernel/../abstract-machine/am/include/am.h \
+ /home/hz/Laser/os-workbench/kernel/../abstract-machine/am/include/arch/x86_64-qemu.h \
+ /home/hz/Laser/os-workbench/kernel/../abstract-machine/am/include/amdev.h \
+ /home/hz/Laser/os-workbench/kernel/../abstract-machine/am/include/amdev.h \
+ /home/hz/Laser/os-workbench/kernel/../abstract-machine/klib/include/klib.h \
+ /home/hz/Laser/os-workbench/kernel/../abstract-machine/klib/include/klib-macros.h
